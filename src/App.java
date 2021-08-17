1c18185d66aecc258e0144847964770af12d1015
Caine <hollyhampster@gmail.com>
2021-08-17 18:23:16 +0100

    First Upload

--- src/App.java
+++ src/App.java
@@ -5,18 +5,25 @@ import javax.swing.*;
 public class App {
     static JFrame frame;
     public static void main(String[] args) throws Exception {
-        frame = new JFrame("Test");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Label label = new Label();
-        label.setText("Eggplant");
-        label.setAlignment(label.CENTER);
-        frame.getContentPane().add(label, BorderLayout.CENTER);
-        frame.pack();
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);;
+        initiateWindow();
+        initiateUI();
+        displayMainScreen();
     }
 
-    void initiateWindow(){
+    static void initiateWindow(){
+        frame = new JFrame("Test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
+    static void initiateUI(){
+        Label label = new Label();
+        label.setText("Eggplant");
+        label.setAlignment(Label.CENTER);
+        frame.getContentPane().add(label, BorderLayout.CENTER);
+    }
+    static void displayMainScreen(){
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
     }
 }
